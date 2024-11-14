cd888c083a9b66fe4725c85a9b299e99ae7a34ce
Solah <solah.kyle.skye@gmail.com>
2024-11-13 19:23:42 -0600

    updated intro page

--- src/UserName.jsx
+++ src/UserName.jsx
@@ -4,20 +4,29 @@ export default function UserName(props) {
   return (
     <>
       <div className="bodyWrap">
-        <div className="welcome">Welcome Adventurer!</div>
+        <div className="welcome">Welcome, Adventurer!</div>
+        <div className="intro">... to our little village of Scales Haven.</div>
         <div className="intro">
-          You’ve journeyed far and wide, but now it’s time to settle down and
-          build a new life in the peaceful village of Scales Haven. As a fledgling
-          adventurer, your days will be filled with exciting tasks — from
-          gathering resources and crafting tools to helping the townsfolk with
-          their daily needs. The more you accomplish, the more gold you’ll earn,
-          and with your hard-earned treasure, you’ll be able to purchase rare
-          and powerful dragon eggs.
+          SO, you've heard about the dragon eggs? And you expect to collect as
+          many as you can to raise a dragon army? Dangerous, dumb, but
+          understandable!
         </div>
-        <div className="intro">What do you say?  Would you like to start your own brood?</div>
-        <div className="intro"></div>
+        <div className="intro">
+          Not so fast, though! You have to earn your keep before the dragons
+          will let you have even one of their precious eggs!
+        </div>
+        <div className="intro">
+          ALL Brood Leaders, even seasoned ones, are expected to complete their
+          daily routines, as well as a few additional quests to get and keep
+          their eggs. Be consistent enough and a dragon egg could be
+          yours!{" "}
+        </div>
+        <div className="intro">
+          Although, then you have to figure out what to do with it...
+        </div>
         <div className="foot">
-          Tell us, adventurer, what is your name?
+          If you are ready to accept the risks, go ahead and enter your name
+          below!
         </div>
       </div>
     </>
