809b1e908efa16ed4715c641c9596b2945c536e6
Solah <solah.kyle.skye@gmail.com>
2024-12-21 13:31:02 -0600

    className fixed on loading indicator

--- src/components/LandingPage/LoadingIndicator.jsx
+++ src/components/LandingPage/LoadingIndicator.jsx
@@ -3,7 +3,7 @@ import "./spinkit.css"
 export const LoadingIndicator = () => {
     return <div> 
 
-<div classname="loadingIndicator">
+<div className="loadingIndicator">
     <div className="sk-chase">
       <div className="sk-chase-dot"></div>
       <div className="sk-chase-dot"></div>
