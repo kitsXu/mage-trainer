e4fd1e93381c9e8c0884bb2df0f22a563af5cdaa
Solah <solah.kyle.skye@gmail.com>
2024-12-22 00:36:54 -0600

    WIP- Market Place Egg and Item views rendered
    -set up basic store format
    -purchase form/buttons for eggs set up
    
    TODOs-
    -write code for purchase buttons/hook up to gold
    -make items
    -style css
    -

---
+++ src/components/Market/Items.jsx
@@ -0,0 +1,14 @@
+export default function Items(props) {
+
+    // -- [ ] create div to hold dragon eggs
+    // -- [ ] create div to display facts about dragon eggs obtained
+    //  - [ ] creeate div to hold items
+  
+  
+    
+      return (
+        <div className="profile">
+          <div className="divider">__________</div>
+        </div>
+      );
+    }
--- src/components/Market/Market.jsx
+++ src/components/Market/Market.jsx
@@ -1,26 +1,27 @@
 import React from "react";
-import earthEgg from './images/earthEgg.png'
-import lavaEgg from './images/lavaEgg.png'
-import './market.css'
+import { useState } from "react";
+import "./market.css";
+import Eggs from "./Eggs.jsx";
+import Items from "./Items.jsx";
 
 export default function Market(props) {
+  const [view, setView] = useState("eggs");
 
-    // -- [ ] create div to hold dragon eggs for sale! images?
-    //  - [ ] separate div for egg prices?
-    // -- [ ] create div to hold items for sale!
-    //  - [ ] 
-  
-    
-      return (
-        <div className="profile">
-          <h1 className="userHeader">Scales Haven Marketplace</h1>
-          <div className="earthDiv">
-            <img src={earthEgg} className="earthEgg"/>
-          </div>
-          <div className="lavaDiv">
-            <img src={lavaEgg} className="lavaEgg"/>
-          </div>
-          <div className="divider">__________</div>
-        </div>
-      );
-    }
+  return (
+    <div className="profile">
+      <h1 className="userHeader">Scales Haven Marketplace</h1>
+      <div className="marketButtons">
+        <button className="menuBtn" onClick={() => setView("eggs")}>
+          Buy Eggs
+        </button>
+        <button className="menuBtn" onClick={() => setView("items")}>
+          Buy Items
+        </button>
+      </div>
+      <div>
+        {view === "eggs" && <Eggs />}
+        {view === "items" && <Items />}
+      </div>
+    </div>
+  );
+}
