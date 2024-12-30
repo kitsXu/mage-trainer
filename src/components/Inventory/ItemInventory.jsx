52a24767f7931bc396eff02b0dc61195931cec9d
Solah <solah.kyle.skye@gmail.com>
2024-12-29 22:45:28 -0600

    inventory and brood pages set up

---
+++ src/components/Inventory/ItemInventory.jsx
@@ -0,0 +1,15 @@
+import './inventory.css'
+
+export default function ItemInventory(props) {
+
+
+  return (
+    <div className="profile">
+      <h1 className="item-brood-Header">Items- ({0})</h1>
+      <div className="divider">_________</div>
+      <div className="logo">
+        <a className="logo-tag-brood" href="https://ko-fi.com/kitsxu">-kitsXu-</a>
+      </div>
+    </div>
+  );
+}
