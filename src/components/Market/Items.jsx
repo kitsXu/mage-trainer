775c96895b37ab34aec4c2d8d241b99123543840
Solah <kitsxu.apps@gmail.com>
2025-01-25 21:32:08 -0600

    item page updated
    -item objects made
    -item.jsx elements written to dynamically render items

--- src/components/Market/Items.jsx
+++ src/components/Market/Items.jsx
@@ -1,46 +1,59 @@
 import "./item.css";
 import "./market.css";
-import healthPotion from "./images/healthPotion.png";
-import manaPotion from "./images/manaPotion.png";
-import speedPotion from "./images/speedPotion.png";
+import { items } from "./itemData.js";
+import { useState } from "react";
 
 export default function Items(props) {
+  //--dynamic state for selecting quantities of items.
+  const [itemQtys, setItemQtys] = useState(
+    items.reduce((acc, items) => {
+      acc[items.id] = 0;
+      return acc;
+    }, {})
+  );
+
+  //-- handle changing the quantity input.
+  const handleChange = (e, itemId) => {
+    setItemQtys((prev) => ({
+      ...prev,
+      [itemId]: parseInt(e.target.value) || 0,
+    }));
+  };
+
   return (
     <div className="itemProfile">
       <h1 className="userHeader">Verdant Vial Apothecary</h1>
-
-      <label>
-        Health Potion
-        <div className="itemDiv">
-          <img src={healthPotion} className="itemIcons" />
-          <p className="cost">75G Per</p>
-          <input className="itemAmount" type="number" />
-          <button className="itemPurchase">Purchase</button>
-        </div>
-      </label>
-      <div className="divider">_________</div>
-      <label>
-        Mana Potion
-        <div className="itemDiv">
-          <img src={manaPotion} className="itemIcons" />
-          <p className="cost">75G Per</p>
-          <input className="itemAmount" type="number" />
-          <button className="itemPurchase">Purchase</button>
-        </div>
-        <hr />
-      </label>
-      <div className="divider">_________</div>
-      <label>
-        Speed Potion
-        <div className="itemDiv">
-          <img src={speedPotion} className="itemIcons" />
-          <p className="cost">75G Per</p>
-          <input className="itemAmount" type="number" />
-          <button className="itemPurchase">Purchase</button>
-        </div>
-        <hr />
+      <div className="itemProfile">
+        {items.map((item) => (
+          <div className="itemProfile" key={item.id}>
+            <label>
+              {item.name}
+              <div className="itemDiv">
+                <img src={item.image} className="item" />
+                <p className="cost">{item.cost}G Per</p>
+                <form className="purchaseWrap">
+                  <input
+                    className="itemQtys"
+                    type="number"
+                    value={itemQtys[item.id] || 0}
+                    onChange={(e) => handleChange(e, item.id)}
+                    min="0"
+                    max="100"
+                    step="1"
+                  />
+                  <button
+                    className="itemPurchase"
+                    onClick={(e) => purchaseItem(e, item.id, item.cost)}
+                  >
+                    Purchase
+                  </button>
+                </form>
+              </div>
+            </label>
+          </div>
+        ))}
         <div className="divider">_________</div>
-      </label>
+      </div>
     </div>
   );
 }
