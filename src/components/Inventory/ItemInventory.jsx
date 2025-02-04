9767041e862ad51d0535b6aad5e9ed080224ed6d
Solah <kitsxu.apps@gmail.com>
2025-02-03 22:51:13 -0600

    WIP -starting pulling scrollInv local storage to inventory page to display

--- src/components/Inventory/ItemInventory.jsx
+++ src/components/Inventory/ItemInventory.jsx
@@ -6,25 +6,16 @@ export default function ItemInventory() {
 
  
     //-- Retrieve the array from localStorage.
-const scrollInv = JSON.parse(localStorage.getItem(scrollData));
+const scrollsInv = JSON.parse(localStorage.getItem('scrollInv'));
 
-console.log('ITEM INVENTORY:', scrollData);
-
-//-- Count occurrences of each name.
-// const nameCount = scrollInv.reduce((acc, obj) => {
-
-//   acc[obj.name] = (acc[obj.name] || 0) + 1;
-//   return acc;
-// }, {});
-// console.log(nameCount);
+console.log('ITEM INVENTORY:', scrollsInv);
 
 
 
 
   return (
     <div className="profile">
-      <h1 className="item-brood-Header">Items- ({0})</h1>
-      {/* <div>{nameCount}</div> */}
+      <h1 className="item-brood-Header">Items- (0)</h1>
       <div className="divider">_________</div>
       <div className="logo">
         <a className="logo-tag-item" href="https://ko-fi.com/kitsxu">-kitsXu-</a>
