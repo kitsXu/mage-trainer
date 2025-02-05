f1c22de80a2637966fca661dc16f6e93a184cd78
Solah <kitsxu.apps@gmail.com>
2025-02-04 22:55:03 -0600

    WIP-- getting scrolls from local storage and rendering them in item Inventory

--- src/components/Inventory/ItemInventory.jsx
+++ src/components/Inventory/ItemInventory.jsx
@@ -1,14 +1,25 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './inventory.css'
-import scrollData from '../Market/Scrolls.jsx'
 
 export default function ItemInventory() {
-
+const [storedScrolls, setStoredScrolls] = useState();
  
     //-- Retrieve the array from localStorage.
-const scrollsInv = JSON.parse(localStorage.getItem('scrollInv'));
+// const scrollsInv = JSON.parse(localStorage.getItem('scrollInv'));
 
-console.log('ITEM INVENTORY:', scrollsInv);
+// console.log('ITEM INVENTORY:', scrollsInv);
+
+
+
+useEffect(() => {
+  const scrollInv = localStorage.getItem('scrollInv');
+
+  if (scrollInv) {
+    setStoredScrolls(JSON.parse(scrollInv));
+  }
+  console.log('ITEM INVENTORY:', scrollInv);
+
+}, []);
 
 
 
@@ -17,9 +28,16 @@ console.log('ITEM INVENTORY:', scrollsInv);
     <div className="profile">
       <h1 className="item-brood-Header">Items- (0)</h1>
       <div className="divider">_________</div>
+      {storedScrolls ? (<div>
+        <ul>
+          {Object.keys(storedScrolls).map((scrollInfo) => ( <li key={scrollInfo}>
+            {scrollInfo}:{storedScrolls[scrollInfo]}
+          </li> ))}
+        </ul>
+      </div>) : ( <p>No Scrolls.  Head to the Market to purchase!</p>) }
       <div className="logo">
         <a className="logo-tag-item" href="https://ko-fi.com/kitsxu">-kitsXu-</a>
       </div>
-    </div>
-  );
+    </div> 
+  ); 
 }
