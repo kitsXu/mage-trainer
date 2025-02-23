38979a8affe12ec35d65bda291ac1b0c82ca2117
Solah <kitsxu.apps@gmail.com>
2025-02-22 23:58:13 -0600

    Style Updates

--- src/components/LandingPage/SetUsername.jsx
+++ src/components/LandingPage/SetUsername.jsx
@@ -23,28 +23,27 @@ export default function SetUsername(props) {
   return (
     <>
       <div className="landingBodyWrap">
-        <div className="welcome">Greetings new pupil! </div>
+        <div className="welcome">Welcome! </div>
         <div className="intro">
-          Welcome to the Eldergrove Tower of Advanced Sorcery.
+          ...to the Eldergrove Tower of Advanced Sorcery.
         </div>
         <div className="intro">
-          I must preface your journey with the knowledge that this is no mere
-          school. The Eldergrove Tower is a living entity, older than the grove
+          The Eldergrove Tower is a living entity that is a part of the grove
           that surrounds it. Here, we do not merely study magic; we combine it
           with nature and become a part of it.
         </div>
         <div className="intro">
-          But beware—magic is no tame beast. It rewards those who respect it and
-          devours those who do not. You will be tested. You will be required to
-          be consistnt and disciplined, and the Eldergrove will watch. Secrets
-          few dare to dream of await, if you are able to prove yourself worthy.
+          If you join the ranks of the Eldergrove, you will be tested. You will
+          be required to be consistnt and disciplined, and the Eldergrove will
+          watch. Secrets few dare to dream of await, if you are able to prove
+          yourself worthy.
         </div>
         <div className="intro">
-          Now, step forward. Speak your name so that the Eldergrove may know you
-          and you may begin your journey.
+          If you are ready to take on the challenge, step forward and speak your
+          name to begin!
         </div>
         <div className="inputBarInstruct">
-          Give Your Name to the ElderGrove...
+          Give Your Name to the Eldergrove...
         </div>
         <form
           className="nameInput"
