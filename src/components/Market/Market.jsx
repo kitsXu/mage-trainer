3e8768421a4fd6f669793f3e9d085e3585afa56e
Solah <kitsxu.apps@gmail.com>
2025-03-02 20:35:46 -0600

    Cost Updates- increases after first purchase

--- src/components/Market/Market.jsx
+++ src/components/Market/Market.jsx
@@ -4,15 +4,12 @@ import { scrolls } from "./Data/scrollsData.js";
 import "./market.css";
 
 export default function Market(props) {
-  //--dynamic state for selecting quantities of scrolls.
-  const [scrollQtys, setScrollQtys] = useState(
-    scrolls.reduce((acc, scrolls) => {
-      acc[scrolls.id] = 0;
-      return acc;
-    }, {})
-  );
   const [visibility, setVisibility] = useState(false);
   const [marketMoney, setMarketMoney] = useState(props.user.gold);
+  const [newCost, setNewCost] = useState(() => {
+    const currentScrollCost = localStorage.getItem("scrolls");
+    return currentScrollCost ? JSON.parse(currentScrollCost) : scrolls;
+  });
 
   console.log(marketMoney);
 
@@ -26,58 +23,60 @@ export default function Market(props) {
     props.setGold(marketMoney);
   }, [marketMoney]);
 
-  //-- handle changing the quantity input.
-  const handleChange = (e, scrollId) => {
-    setScrollQtys((prev) => ({
-      ...prev,
-      [scrollId]: parseInt(e.target.value) || 0,
-    }));
-  };
+  // const updatedScrollCost = () => {
+  //   setNewCost((prevNewCost) => prevNewCost.map((newCost) => ({
+  //     ...newCost,
+  //     cost: scrolls.cost + 10000,
+
+  //   })) )
+  // }
 
   //--handle the purchase.
-  const purchaseScroll = (e, scrollId, scrollCost) => {
+  const purchaseScroll = (e, scrollId, currentScrollCost) => {
     e.preventDefault();
-    const amount = scrollQtys[scrollId];
     var currentMoney = marketMoney;
 
-    if (amount === 0) {
-      alert("Please Select Quantity");
+    //--handles the gold in local storage.
+    if (currentMoney >= currentScrollCost > 0) {
+      currentMoney -= currentScrollCost;
+      alert("Purchase complete!  Go check your inventory!");
+      console.log(
+        `PURCHASE COMPLETE for ${currentScrollCost} ${scrollId} --`,
+        props.user
+      );
+      setMarketMoney(currentMoney);
+
+      //-- get stored inventory array from local storage.
+      //-- create new object from scrollData.
+      //-- add new object to array.
+      //-- again set to local storage.
+
+      let scrollInv = JSON.parse(localStorage.getItem("scrollInv")) || [];
+
+      const uniqueId = crypto.randomUUID();
+      const scrollKey = `${scrollId}-${uniqueId}`;
+
+      const newScroll = {
+        id: scrollKey,
+        name: scrollId,
+        cost: currentScrollCost,
+      };
+      scrollInv.push(newScroll);
+
+      localStorage.setItem("scrollInv", JSON.stringify(scrollInv));
+
+      const updatedCost = newCost.map((scrolls) => ({
+          ...scrolls,
+          cost: scrolls.cost + 9950,
+        }))
+      setNewCost(updatedCost);
+  
+
+      localStorage.setItem("scrolls", JSON.stringify(updatedCost));
+
+      console.log("new cost", newCost);
     } else {
-      //--handles the gold in local storage.
-      if (currentMoney >= scrollCost * amount && amount > 0) {
-        currentMoney -= scrollCost * amount;
-        alert("Purchase complete!  Go check your inventory!");
-        console.log(
-          `PURCHASE COMPLETE for ${amount} ${scrollId} --`,
-          props.user
-        );
-        setMarketMoney(currentMoney);
-
-        //-- get stored inventory array from local storage.
-        //-- create new object from scrollData.
-        //-- add new object to array.
-        //-- again set to local storage.
-
-        let scrollInv = JSON.parse(localStorage.getItem("scrollInv")) || [];
-
-        const uniqueId = crypto.randomUUID();
-        const scrollKey = `${scrollId}-${uniqueId}`;
-
-        const newScroll = {
-          id: scrollKey,
-          name: scrollId,
-          amount: amount,
-          cost: scrollCost,
-        };
-        scrollInv.push(newScroll);
-
-        localStorage.setItem("scrollInv", JSON.stringify(scrollInv));
-        console.log(scrollCost);
-
-      } else {
-        console.log("SCROLL QTY");
-        alert("Not Enough Gold!");
-      }
+      alert("Not Enough Gold!");
     }
   };
 
@@ -111,17 +110,8 @@ export default function Market(props) {
             {scroll.name}
             <div className="scrollDiv">
               <img src={scroll.image} className="scroll" />
-              <p className="cost">{scroll.cost}G Per</p>
+              <p key={scroll.id} className="cost">{newCost[0]?.cost}G Per</p>
               <form className="purchaseWrap">
-                <input
-                  className="scrollQtys"
-                  type="number"
-                  value={scrollQtys[scroll.id] || 0}
-                  onChange={(e) => handleChange(e, scroll.id)}
-                  min="0"
-                  max="100"
-                  step="1"
-                />
                 <button
                   className="scrollPurchase"
                   onClick={(e) => purchaseScroll(e, scroll.id, scroll.cost)}
