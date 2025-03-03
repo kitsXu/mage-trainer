88f166954e4ff563ef4e4888ca7ac7d1927d3958
Solah <kitsxu.apps@gmail.com>
2025-03-02 20:49:41 -0600

    updated about page

---
+++ src/components/About/About.jsx
@@ -0,0 +1,20 @@
+import "./about.css";
+
+// -- TO DO --
+// - [ ]
+
+export default function About(props) {
+  return (
+    <div className="profile">
+      <div className="headDivider">§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§</div>{" "}
+
+ 
+      <div className="divider">_________</div>
+      <div className="logo">
+        <a className="logo-tag-archive" href="https://ko-fi.com/kitsxu">
+          -kitsXu-
+        </a>
+      </div>
+    </div>
+  );
+}
